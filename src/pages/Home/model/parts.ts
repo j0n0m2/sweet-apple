d4c9d2a1f2490747513946bfd1f7f66e3ebcdee2
j0n0m2 <dbsk8223@naver.com>
2025-07-07 18:33:47 +0900

    fix : 눈, 입 기본 위치 수정

--- src/pages/Home/model/parts.ts
+++ src/pages/Home/model/parts.ts
@@ -2,7 +2,7 @@ import type { Part } from '@/pages/home/model/type';
 
 // 눈, 입 각 파트 default 위치 및 default 크기 설정
 export const initialParts: Part[] = [
-  { name: 'leftEye', posX: 50, posY: 200, width: 180, height: 50 },
-  { name: 'rightEye', posX: 320, posY: 200, width: 180, height: 50 },
-  { name: 'mouth', posX: 220, posY: 300, width: 120, height: 50 },
+  { name: 'leftEye', posX: 90, posY: 250, width: 180, height: 50 },
+  { name: 'rightEye', posX: 320, posY: 250, width: 180, height: 50 },
+  { name: 'mouth', posX: 240, posY: 380, width: 120, height: 50 },
 ];
