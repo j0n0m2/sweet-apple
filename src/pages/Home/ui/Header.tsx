73ec1f4436cf11f9d1f35ca6e621d3135a568f61
j0n0m2 <dbsk8223@naver.com>
2025-07-07 17:18:42 +0900

    style : 캔버스 요소가 텍스트 뒤로 가도 드래그 될 수 있도록 텍스트 pointer-events-none으로 설정

--- src/pages/Home/ui/Header.tsx
+++ src/pages/Home/ui/Header.tsx
@@ -1,13 +1,13 @@
 const Header = () => {
   return (
     <>
-      <div className="text-center font-['Inter'] text-[220px] leading-80 font-black text-white [text-shadow:_-3px_-3px_0_black,_3px_-3px_0_black,_-3px_3px_0_black,_3px_3px_0_black]">
+      <div className="text-center font-['Inter'] text-[220px] leading-80 font-black text-white select-none [text-shadow:_-3px_-3px_0_black,_3px_-3px_0_black,_-3px_3px_0_black,_3px_3px_0_black]">
         How <b className="italic">Sweet</b>
       </div>
-      <div className="absolute bottom-[0%] left-1/2 z-2 w-[70%] -translate-x-1/2 text-center font-['Inter'] text-[220px] leading-80 font-black text-white [text-shadow:_-3px_-3px_0_black,_3px_-3px_0_black,_-3px_3px_0_black,_3px_3px_0_black]">
+      <div className="pointer-events-none absolute bottom-[0%] left-1/2 z-99 w-[70%] -translate-x-1/2 text-center font-['Inter'] text-[220px] leading-80 font-black text-white select-none [text-shadow:_-3px_-3px_0_black,_3px_-3px_0_black,_-3px_3px_0_black,_3px_3px_0_black]">
         My <b className="italic">Apple</b>
       </div>
-      <div className="absolute bottom-10 left-1/2 h-50 w-260 -translate-x-1/2 rounded-[100%] bg-[#11ff00]"></div>
+      <div className="absolute bottom-10 left-1/2 z-1 h-50 w-260 -translate-x-1/2 rounded-[100%] bg-[#11ff00]"></div>
     </>
   );
 };
