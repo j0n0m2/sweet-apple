70f296a7c76c53882e944893bb96c403f70ebbc9
j0n0m2 <dbsk8223@naver.com>
2025-07-14 14:01:24 +0900

    refactor : 더미 데이터 타입 정의

--- src/sections/market/index.tsx
+++ src/sections/market/index.tsx
@@ -1,4 +1,9 @@
-const dummyData = [
+interface Item {
+  name: string;
+  price: number;
+}
+
+const dummyData: Item[] = [
   { name: 'Apple', price: 1.2 },
   { name: 'Banana', price: 0.8 },
   { name: 'Cherry', price: 2.5 },
@@ -7,10 +12,10 @@ const dummyData = [
 const Market = () => {
   return (
     <div>
-      <h1>Market Page</h1>
+      <h1>사과 나눔합니다</h1>
       <ul className="flex flex-wrap gap-24">
         {dummyData.map((item, index) => (
-          <li key={index} className="border-1 p-4">
+          <li key={index} className="rounded-lg border-1 p-4">
             <div>Name: {item.name}</div>
             <div>Price: ${item.price.toFixed(2)}</div>
           </li>
