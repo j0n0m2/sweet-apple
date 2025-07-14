4334effd012cd1ef15b7c3c118a6172c59d17264
j0n0m2 <dbsk8223@naver.com>
2025-07-14 20:37:52 +0900

    style : Header 스타일 수정 - 배경 요소 제거

--- src/sections/main/ui/Header.tsx
+++ src/sections/main/ui/Header.tsx
@@ -1,11 +1,10 @@
 const Header = () => {
   return (
-    <div className="p-10">
+    <div className="inline-block p-10">
       <div className="font-['Inter'] text-[56px] leading-16 font-black text-white select-none [text-shadow:_-1px_-1px_0_black,_1px_-1px_0_black,_-1px_1px_0_black,_1px_1px_0_black]">
         How <b className="italic">Sweet</b>
       </div>
-      <div className="absolute mt-4 h-12 w-60 rounded-[100%] bg-[#11ff00]"></div>
-      <div className="pointer-events-none absolute w-full font-['Inter'] text-[56px] leading-16 font-black text-white select-none [text-shadow:_-1px_-1px_0_black,_1px_-1px_0_black,_-1px_1px_0_black,_1px_1px_0_black]">
+      <div className="pointer-events-none inline-block rounded-[100%] bg-[#11ff00] font-['Inter'] text-[56px] leading-14 font-black text-white select-none [text-shadow:_-1px_-1px_0_black,_1px_-1px_0_black,_-1px_1px_0_black,_1px_1px_0_black]">
         My <b className="italic">Apple</b>
       </div>
     </div>
