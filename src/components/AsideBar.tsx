75e1ed064d10d91867875fbfe21064bb18cf3656
j0n0m2 <dbsk8223@naver.com>
2025-07-18 15:40:00 +0900

    refactor : clsx를 활용하여 조건부 클래스 처리 간결화

--- src/components/AsideBar.tsx
+++ src/components/AsideBar.tsx
@@ -1,55 +1,32 @@
-import Market from '@/sections/market';
 import { useState } from 'react';
-import { MENU_ITEMS } from '@/constants/menuItems';
-import { MENU_HOW_TEXT, MENU_ABOUT_TEXT } from '@/constants/text';
-
-const items = [MENU_HOW_TEXT, <Market />, MENU_ABOUT_TEXT];
+import clsx from 'clsx';
+import MENU_DATA from '@/constants/MENU_DATA';
 
 const AsideBar = () => {
-  const [menu, setMenu] = useState(0);
-
-  const handleMenuClick = (index: number) => {
-    setMenu(index);
-  };
+  const [menuIndex, setMenuIndex] = useState(0);
 
   return (
     <div className="z-20 h-full w-full flex-1 border-l border-[#003661] bg-[linear-gradient(270deg,_#ffffff60_0%,_#ffffff_100%)] text-[#003661]">
-      <div>
-        <ul className="flex">
-          {MENU_ITEMS.map((name, index) => {
-            const liClasses = [
-              'flex-1',
-              'cursor-pointer',
-              'border-b',
-              'border-[#003661]',
-              'p-4',
-              'text-center',
-              'text-[32px]',
-            ];
-
-            if (index === 1) {
-              liClasses.push('border-x');
-            }
-
-            if (index === menu) {
-              liClasses.push('border-b-0');
-            } else {
-              liClasses.push('border-b', 'bg-[#c0e3ff]', 'text-[#2c6fa6]');
-            }
-
-            return (
-              <li
-                key={name}
-                className={liClasses.join(' ')}
-                onClick={() => handleMenuClick(index)}
-              >
-                {name}
-              </li>
-            );
-          })}
-        </ul>
+      <ul className="flex">
+        {MENU_DATA.map(({ name }, index) => (
+          <li
+            key={name}
+            className={clsx(
+              'flex-1 cursor-pointer border-b border-[#003661] p-4 text-center text-[32px]',
+              index === 1 && 'border-x',
+              index === menuIndex
+                ? 'border-b-0'
+                : 'border-b bg-[#c0e3ff] text-[#2c6fa6]'
+            )}
+            onClick={() => setMenuIndex(index)}
+          >
+            {name}
+          </li>
+        ))}
+      </ul>
+      <div className="p-8 text-[24px] whitespace-pre-line">
+        {MENU_DATA[menuIndex].content}
       </div>
-      <div className="p-8 text-[24px] whitespace-pre-line">{items[menu]}</div>
     </div>
   );
 };
