45653893d5274fb8190108ea4878cd9eff78fc0e
j0n0m2 <dbsk8223@naver.com>
2025-07-21 20:47:24 +0900

    feat : Tanstack Query Devtools 사용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Index from '@/index.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Index />
+      <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   );
 }
