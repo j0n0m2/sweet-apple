ad51b483a5ded7ec61455d550a97eb1bd1518e71
j0n0m2 <dbsk8223@naver.com>
2025-07-24 20:26:07 +0900

    fix : 프로젝트 클론이 용이하도록 API KEY 환경변수로 분리

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDEOYX00UmXZvUyPqgZFkZccL1OCi0I-bI',
-  authDomain: 'sweet-apple-bc1b7.firebaseapp.com',
-  projectId: 'sweet-apple-bc1b7',
-  storageBucket: 'sweet-apple-bc1b7.firebasestorage.app',
-  messagingSenderId: '904025659303',
-  appId: '1:904025659303:web:7bf5cd5dcde9a7b6cc0882',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
